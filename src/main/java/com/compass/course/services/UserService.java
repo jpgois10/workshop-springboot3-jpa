3f0c553c48a2fb69edabae8a0573a9025dbf0ca6
Joao Paulo Barbosa <jpbgois10@gmail.com>
2024-12-24 17:42:38 -0300

    Exception handling - update

--- src/main/java/com/compass/course/services/UserService.java
+++ src/main/java/com/compass/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.compass.course.entities.User;
 import com.compass.course.repositories.UserRepository;
 import com.compass.course.services.exceptions.DatabaseException;
 import com.compass.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+       try {
+           User entity = userRepository.getReferenceById(id);
+           updateData(entity, obj);
+           return userRepository.save(entity);
+
+       } catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     public void updateData(User entity, User obj) {
