e750b83ca1c79286264ef8d1a074c98404ab7b78
Joao Paulo Barbosa <jpbgois10@gmail.com>
2024-12-24 16:34:25 -0300

    User update

--- src/main/java/com/compass/course/services/UserService.java
+++ src/main/java/com/compass/course/services/UserService.java
@@ -24,12 +24,23 @@ public class UserService {
         return userRepository.findById(id).get();
     }
 
-    public User insert(User obj){
+    public User insert(User obj) {
         return userRepository.save(obj);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         userRepository.deleteById(id);
+    }
 
+    public User update(Long id, User obj) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
+
+    public void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
